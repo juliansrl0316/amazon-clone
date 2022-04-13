51d70ed069a2de0f9822b0fcc96287711b54c3b0
Julian Ramirez <julian09112001@gmail.com>
2022-04-13 14:20:37 -0500

    checkout pages added

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
+This is an amazon clone, i made it for the purpose of practicing React, you can check the demo in the following link:
+- https://amazon-clone-gray-tau.vercel.app/
+
+This is the tutorial with which I did the practice:
+- https://www.youtube.com/watch?v=oq0B7XUNbtk&list=PLf16UKl7nR5AF9k4ckTG82onvpAv1NAE7&index=1
+
+# Installation Steps
+
+
 # Installation Steps
 
 
--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -6,11 +6,30 @@ import { selectItems, selectTotal } from "../slices/basketSlice";
 import Currency from "react-currency-formatter";
 import { useSession } from "next-auth/react";
 import CheckoutProduct from "../components/CheckoutProduct";
+import { loadStripe } from "@stripe/stripe-js";
+import axios from "axios";
+
+const stripePromise = loadStripe(process.env.stripe_public_key);
 
 function Checkout() {
   const items = useSelector(selectItems);
   const total = useSelector(selectTotal);
-  const {data: session} = useSession();
+  const { data: session } = useSession();
+
+  const createCheckoutSession = async () => {
+    const stripe = await stripePromise;
+
+    const checkoutSession = await axios.post("/api/create-checkout-session", {
+      items: items,
+      email: session.user.email,
+    });
+
+    const result = await stripe.redirectToCheckout({
+      sessionId: checkoutSession.data.id,
+    });
+
+    if (result.error) alert(result.error.message);
+  };
 
   return (
     <div className="bg-gray-100">
@@ -48,12 +67,20 @@ function Checkout() {
           {items.length > 0 && (
             <>
               <h2 className="whitespace-nowrap">
-                Subtotal ({items.length} items): {" "}
+                Subtotal ({items.length} items):{" "}
                 {/* <span className="font-bold"> */}
-                  {<Currency className="font-bold" quantity={total} currency="USD" />}
+                {
+                  <Currency
+                    className="font-bold"
+                    quantity={total}
+                    currency="USD"
+                  />
+                }
                 {/* </span> */}
               </h2>
               <button
+                role="link"
+                onClick={createCheckoutSession}
                 disabled={!session}
                 className={`button mt-2 ${
                   !session &&
